6c517176931af155ef2284c6ccff8eabccbad09d
maulahaz <maulahaz@gmail.com>
2023-11-14 23:46:23 +0400

    Create Module Quiz

---
+++ lib/modules/quiz/mdl_quiz.dart
@@ -0,0 +1,15 @@
+class QuizModel {
+  // final int id;
+  // final String title;
+  // final String image;
+
+  // ProductModel(
+  //     {required this.id,
+  //     required this.title,
+  //     required this.image});
+
+  // factory ProductModel.fromModel(Map<String, dynamic> json) => ProductModel(
+  //     id: json['id'],
+  //     title: json['firstName'],
+  //     image: json['lastName']);
+}
